9038da9be213954bd4d408a044e212b39eaa9218
saravanapandianbalan <31887088+saravanapandianbalan@users.noreply.github.com>
2024-04-12 17:52:55 +0530

    asg

--- main.tf
+++ main.tf
@@ -33,18 +33,23 @@ data "aws_vpc" "default" {
   default = true
 }
 
-resource "aws_instance" "blog" {
-  ami           = data.aws_ami.app_ami.id
+module "autoscaling" {
+  source  = "terraform-aws-modules/autoscaling/aws"
+  version = "7.4.1"
+  
+  name = "blog"
+  min_size = 1
+  max_size = 2
+
+  vpc_zone_identifier    = module.blog_vpc.public_subnets
+  target_group_arns      = module.blog_vpc.target_group_arns
+  security_groups        = [module.blog_sec_group.security_group_id]
+
+  ami = data.aws_ami.app_ami.id
   instance_type = var.instance_type
-
-  vpc_security_group_ids = [module.blog_sec_group.security_group_id]
-
-  subnet_id = module.blog_vpc.public_subnets[0]
-
-  tags = {
-    Name = "HelloWorld"
-  }
 }
+
+
 
 module "blog_alb" {
  source = "terraform-aws-modules/alb/aws"
